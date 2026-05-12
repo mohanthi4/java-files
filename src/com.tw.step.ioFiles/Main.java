1e855e228d34e3cc2803370b151da8a9a30e42c6
gajulapallimohanthi <mohanthigajulapalli04@gmail.com>
2026-05-12 23:12:53 +0530

    IMplements multiple files calculation as separate file and have a log

--- src/com.tw.step.ioFiles/Main.java
+++ src/com.tw.step.ioFiles/Main.java
@@ -3,18 +3,17 @@ package com.tw.step.ioFiles;
 import java.io.*;
 import java.nio.file.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 class Main{
     static void main() throws IOException, InterruptedException {
-        List<Integer> currentScore=new ArrayList<>();
         WatchService watcher = FileSystems.getDefault().newWatchService();
         Path path = Paths.get("resources.data/incoming");
+        readInitialScores(path);
         path.register(watcher, ENTRY_MODIFY,ENTRY_CREATE);
         while(true){
             WatchKey action = watcher.take();
@@ -22,37 +21,62 @@ class Main{
                 Path fileName = (Path) event.context();
                 String name = fileName.toString();
                 if (isValidFile(name)) {
-                    int totalScore = getTotalScore(readableFilePath(path, name));
-                    updateCurrentScores(totalScore,currentScore);
+                    performFileOperation(validFileFormat(path, name));
                 }
             }
             if (!action.reset()) break;
         }
     }
 
+    private static void readInitialScores(Path path) throws IOException {
+        try (Stream<Path> stream = Files.list(path)) {
+            stream.filter(Files::isRegularFile)
+                    .forEach(file -> {
+                        try {
+                            performFileOperation(file);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
+    }
+
+    private static void performFileOperation(Path file) throws IOException {
+        int totalScore = getTotalScore(file.toString());
+        updateCurrentScores(file.toString(), totalScore);
+    }
+
     private static boolean isValidFile(String name) {
         return name.startsWith("score_") && name.endsWith(".txt");
     }
 
-    private static Scanner readableFilePath(Path path, String name) throws FileNotFoundException {
-        String filePath= path.toString()+"/"+ name;
-        return new Scanner(new BufferedReader(new FileReader(filePath)));
+    private static Path validFileFormat(Path path, String name) {
+        return  path.resolve(name);
     }
 
-    private static int getTotalScore(Scanner readFileStream)  {
+    private static int getTotalScore(String readFileStream) throws FileNotFoundException {
         int sum = 0;
-        while(readFileStream.hasNext()){
-            sum+=readFileStream.nextInt();
+        try (Scanner scoreFile = new Scanner(new BufferedReader(new FileReader(readFileStream)))) {
+            while (scoreFile.hasNext()) {
+                sum += scoreFile.nextInt();
+            }
         }
         return sum;
     }
 
-    private static void updateCurrentScores(int totalScore,List<Integer> currentScore) throws IOException {
-        try(BufferedOutputStream bufferedWriter = new BufferedOutputStream(new FileOutputStream("resources.data/scores.txt",true))){
-            LocalDateTime currentTime = LocalDateTime.now();
-            currentScore.add(totalScore);
-            bufferedWriter.write(currentScore.getLast());
+    private static void updateCurrentScores(String file, int totalScore) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("resources.data/scores.txt", true))) {
+            LocalDateTime now = LocalDateTime.now();
+            writeALine(writer, String.valueOf(now),"updated time : ");
+            writeALine(writer, String.valueOf(totalScore), file +": ");
         }
+
+    }
+
+    private static void writeALine(BufferedWriter writer, String now, String tag) throws IOException {
+        writer.write(tag);
+        writer.write(now);
+        writer.newLine();
     }
 
 
